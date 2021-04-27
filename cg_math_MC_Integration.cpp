ca04feff77c72fcea67b8daeb630eebdb339d4e5
Aditya Choubey <aditya-c2512@users.noreply.github.com>
2021-04-27 21:26:34 +0530

    Added Monte Carlo Integration for any function with Uniform PDF

---
+++ cg_math_MC_Integration.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float f(float x)
+{
+    float val = 2*x*x + x;
+}
+
+int main()
+{
+    long long samples = 1000000;
+    float a = 2.0f, b = 10.0f;
+    float S = 0.0f;
+    for(long long i = 0; i < samples; i++)
+    {
+        float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+        float x = a + r*(b-a);
+        float fx = f(x);
+        S += fx * (b-a);
+    }
+    
+    cout << "MC Integration of f(x) from a to b is : " << S/samples << endl;
+}
